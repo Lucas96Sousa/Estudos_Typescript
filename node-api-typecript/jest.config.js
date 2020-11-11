f72402e6bfc24d3af1e5488be69e42ca96b07f66
Lucas Sousa <Lucas996oliveira@gmail.com>
2020-11-11 17:35:39 +0000

    correção dos arquivos do jest.

--- node-api-typecript/jest.config.js
+++ node-api-typecript/jest.config.js
@@ -6,7 +6,7 @@ const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
   displayName: 'root-tests',
-  testMatch: ['<rootDir>/src/**/*.tests.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
   testEnvironment: 'node',
   clearMocks: true,
   preset: 'ts-jest',
