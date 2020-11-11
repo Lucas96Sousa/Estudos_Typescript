ef1aac448e3af97bcee6e0923bf76f7663ba47ff
Lucas Sousa <Lucas996oliveira@gmail.com>
2020-11-11 19:49:12 +0000

    Teste de api finalizado.

--- node-api-typecript/src/clients/stormGlass.ts
+++ node-api-typecript/src/clients/stormGlass.ts
@@ -1,8 +1,13 @@
-
+import axios from 'axios'
 
 export class StormGlass {
-
+  readonly stormGlassApiParams = 
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
+  
+    readonly stormGlassApiSource= 'noaa'
+  // eslint-disable-next-line @typescript-eslint/ban-types
   public async fetchPoints(lat: number, lng: number): Promise<{}>{
-      return Promise.resolve({})
+    return axios.get(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}&end=1592113802&lat=${lat},lng=${lng}`)
   }
 }
+
