039a53cf49c1cf7b7695c0a24ca59d8998242ce0
Lucas Sousa <Lucas996oliveira@gmail.com>
2020-11-18 20:31:12 -0300

    chore: refactor tests

--- clean-node-api/jest.config.js
+++ clean-node-api/jest.config.js
@@ -1,11 +1,19 @@
-export default {
-  roots: ['<rootDir>/src/'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**',
+  ],
   coverageDirectory: 'coverage',
-
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
   },
 };
