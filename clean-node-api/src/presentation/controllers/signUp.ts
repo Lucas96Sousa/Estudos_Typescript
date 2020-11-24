3da82648294770992385d2539ccd5d961419d9af
Lucas Sousa <Lucas996oliveira@gmail.com>
2020-11-23 22:47:41 -0300

    refactor: simplify code

--- clean-node-api/src/presentation/controllers/signUp.ts
+++ clean-node-api/src/presentation/controllers/signUp.ts
@@ -1,16 +1,17 @@
-import { HttpResponse, HttpRequest } from '../protocols/http'
-
 import { MissingParamError } from '../error/missing-param-error'
 
+import { HttpResponse, HttpRequest } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
+  // @ts-expect-error
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
