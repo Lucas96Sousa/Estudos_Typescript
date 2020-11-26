23fcdcc033d70d988647e9a4dd3cd0029990e732
lucas96sousa <lucas996oliveira@gmail.com>
2020-11-26 14:52:23 -0300

    chore: add jest configs and scripts

---
+++ clean-node-api/jest-integration-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config');
+config.testMatc = ['**/*.test.ts'];
+module.exports = config;
---
+++ clean-node-api/jest-unit-config.js
@@ -0,0 +1,4 @@
+const config = require('./jest.conifg');
+
+config.testMatch = ['**/*.spec.ts'];
+module.expors = config;
