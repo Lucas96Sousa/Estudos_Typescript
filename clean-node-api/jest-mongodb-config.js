aab16082eb8bb2f9bbc6ff8b30a5bc2775300d1c
lucas96sousa <lucas996oliveira@gmail.com>
2021-02-10 16:48:14 -0300

    test mongodb

--- clean-node-api/jest-mongodb-config.js
+++ clean-node-api/jest-mongodb-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mongodbMemoryServerOptions: {
+    binary: {
+      version: 'latest',
+      skipMD5: true,
+    },
     instance: {
       dbName: 'jest',
-    },
-    binary: {
-      version: '4.0.3',
-      skipMD5: true,
     },
     autoStart: false,
   },
