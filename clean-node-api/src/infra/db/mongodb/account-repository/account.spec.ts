46c259ecd201daf408e53b3d647f3009797761a2
lucas96sousa <lucas996oliveira@gmail.com>
2021-02-15 15:44:35 -0300

    refactor: clear database between tests

--- clean-node-api/src/infra/db/mongodb/account-repository/account.spec.ts
+++ clean-node-api/src/infra/db/mongodb/account-repository/account.spec.ts
@@ -8,6 +8,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getColletction('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
