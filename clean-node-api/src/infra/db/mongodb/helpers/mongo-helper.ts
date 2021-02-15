5b2a655ee1cd1a3b0978022c5d783154c445a214
lucas96sousa <lucas996oliveira@gmail.com>
2021-02-15 15:41:21 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- clean-node-api/src/infra/db/mongodb/helpers/mongo-helper.ts
+++ clean-node-api/src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,7 +1,7 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
-  client: null as MongoClient,
+  client: null as unknown as MongoClient,
 
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
@@ -12,5 +12,9 @@ export const MongoHelper = {
 
   async disconnect () {
     await this.client.close()
+  },
+
+  getColletction (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
