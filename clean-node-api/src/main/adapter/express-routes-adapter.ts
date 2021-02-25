aba950bfe1979a4d4bafd3b9b9e78ec8febe2d20
lucas96sousa <lucas996oliveira@gmail.com>
2021-02-25 17:01:15 -0300

    adding express router adapter to use on signUp routes

---
+++ clean-node-api/src/main/adapter/express-routes-adapter.ts
@@ -0,0 +1,3 @@
+import { Controller } from "../../presentation/protocols";
+
+export const adaptRoute = (controller: Controller) => { }
--- clean-node-api/src/main/routes/signup-routes.ts
+++ clean-node-api/src/main/routes/signup-routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router): void => {
   router.post('/signup', (req, res) => {
