e852320fa68dfd31aeef991d9d24208a2e3ad55d
lucas96sousa <lucas996oliveira@gmail.com>
2021-05-03 14:45:09 -0300

    adding .env to ignore

--- clean-node-api/src/infra/db/mongodb/helpers/mongo-helper.ts
+++ clean-node-api/src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,22 +4,22 @@ import { AccountModel } from '../../../../domain/models/account'
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
-  async connect (uri: string): Promise<void> {
+  async connect(uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
 
-  async disconnect () {
+  async disconnect() {
     await this.client.close()
   },
 
-  getColletction (name: string): Collection {
+  getColletction(name: string): Collection {
     return this.client.db().collection(name)
   },
 
-  map (collection: any): any {
+  map(collection: any): any {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id })
   }
